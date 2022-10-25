ebc90d16497ec5361c684a7bbc096e2ed723957c
airy-swift <thanks.xa@gmail.com>
2022-10-25 20:55:42 +0900

    make swagger client

---
+++ lib/gen/api/swagger.swagger.dart
@@ -0,0 +1,110 @@
+import 'swagger.models.swagger.dart';
+import 'package:chopper/chopper.dart';
+
+import 'client_mapping.dart';
+import 'dart:async';
+import 'package:chopper/chopper.dart' as chopper;
+export 'swagger.models.swagger.dart';
+
+part 'swagger.swagger.chopper.dart';
+
+// **************************************************************************
+// SwaggerChopperGenerator
+// **************************************************************************
+
+@ChopperApi()
+abstract class Swagger extends ChopperService {
+  static Swagger create({
+    ChopperClient? client,
+    Authenticator? authenticator,
+    String? baseUrl,
+    Iterable<dynamic>? interceptors,
+  }) {
+    if (client != null) {
+      return _$Swagger(client);
+    }
+
+    final newClient = ChopperClient(
+        services: [_$Swagger()],
+        converter: $JsonSerializableConverter(),
+        interceptors: interceptors ?? [],
+        authenticator: authenticator,
+        baseUrl: baseUrl ?? 'http://');
+    return _$Swagger(newClient);
+  }
+
+  ///ひらがな
+  Future<chopper.Response<Conversion>> hiraganaPost(
+      {required ConversionRequest? body}) {
+    generatedMapping.putIfAbsent(Conversion, () => Conversion.fromJsonFactory);
+
+    return _hiraganaPost(body: body);
+  }
+
+  ///ひらがな
+  @Post(path: '/hiragana')
+  Future<chopper.Response<Conversion>> _hiraganaPost(
+      {@Body() required ConversionRequest? body});
+}
+
+typedef $JsonFactory<T> = T Function(Map<String, dynamic> json);
+
+class $CustomJsonDecoder {
+  $CustomJsonDecoder(this.factories);
+
+  final Map<Type, $JsonFactory> factories;
+
+  dynamic decode<T>(dynamic entity) {
+    if (entity is Iterable) {
+      return _decodeList<T>(entity);
+    }
+
+    if (entity is T) {
+      return entity;
+    }
+
+    if (isTypeOf<T, Map>()) {
+      return entity;
+    }
+
+    if (isTypeOf<T, Iterable>()) {
+      return entity;
+    }
+
+    if (entity is Map<String, dynamic>) {
+      return _decodeMap<T>(entity);
+    }
+
+    return entity;
+  }
+
+  T _decodeMap<T>(Map<String, dynamic> values) {
+    final jsonFactory = factories[T];
+    if (jsonFactory == null || jsonFactory is! $JsonFactory<T>) {
+      return throw "Could not find factory for type $T. Is '$T: $T.fromJsonFactory' included in the CustomJsonDecoder instance creation in bootstrapper.dart?";
+    }
+
+    return jsonFactory(values);
+  }
+
+  List<T> _decodeList<T>(Iterable values) =>
+      values.where((v) => v != null).map<T>((v) => decode<T>(v) as T).toList();
+}
+
+class $JsonSerializableConverter extends chopper.JsonConverter {
+  @override
+  FutureOr<chopper.Response<ResultType>> convertResponse<ResultType, Item>(
+      chopper.Response response) async {
+    if (response.bodyString.isEmpty) {
+      // In rare cases, when let's say 204 (no content) is returned -
+      // we cannot decode the missing json with the result type specified
+      return chopper.Response(response.base, null, error: response.error);
+    }
+
+    final jsonRes = await super.convertResponse(response);
+    return jsonRes.copyWith<ResultType>(
+        body: $jsonDecoder.decode<Item>(jsonRes.body) as ResultType);
+  }
+}
+
+final $jsonDecoder = $CustomJsonDecoder(generatedMapping);
